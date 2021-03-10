71b30b0cd1a70ad38e1fc47ef8b5fa19077dc1ff
Patrick Paiva <patrickpaiva88@gmail.com>
2021-03-10 09:50:31 -0300

    created feature findUserById

--- src/index.js
+++ src/index.js
@@ -54,7 +54,17 @@ function checksTodoExists(request, response, next) {
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  const user = users.find(item => item.id === id)
+
+  if (!user) {
+    return response.status(404).json({error: "user not found!"})
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
